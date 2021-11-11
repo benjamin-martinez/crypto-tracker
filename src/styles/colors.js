a520fc90aaa6b2dfa1c22090cd354bd5a2262336
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-11-11 18:50:17 -0500

    Implemented themes into App.js and removed boilerplate code.

---
+++ src/styles/colors.js
@@ -0,0 +1,29 @@
+export const themes = {
+    dark: {
+        background: "#1f2128",
+        color: "#ffffff",
+        card: {
+            background: "#191b1f",
+            active: "#2c2f36"
+        },
+        chart: {
+            color: "#707070",
+            lineColor: "#00ff5f",
+            barColor: "#2172e5"
+        }
+    },
+    light: {
+        background: "#fcfcfc",
+        color: "#191b1f",
+        card: {
+            background: "#ffffff",
+            active: "#fcfcfc"
+        },
+        chart: {
+            color: "#2c2f36",
+            lineColor: "#2550ea",
+            barColor: "#1ad761"
+        }
+    }
+}
+
