4351249a5afce3bfa62151591ef41bdafc43c4a5
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-11-18 11:38:31 -0500

    Removed console logs and implemented hasError boolean

--- src/components/header/Infographic/Infographic.js
+++ src/components/header/Infographic/Infographic.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { Wrapper, Volume, PriceWrapper, BtcDominance, Exchanges, MarketCap, Icon, EthDominance, InnerWrapper, CoinsExchangesWrapper, Coins } from "./Infographic.styles"
 import { DownArrow, NeutralDot, UpArrow } from "styles/arrows";
 import { NavText } from "styles/Fonts";
 import { Slider, SliderWrapper } from "styles/sliders";
@@ -21,9 +20,9 @@ export default class Infographic extends React.Component {
         try {
             this.setState({isLoading: true})
             const { data } = await axios("https://api.coingecko.com/api/v3/global");
-            console.log(data.data)
             this.setState({
                 isLoading: false,
+                hasError: false,
                 numCoins: data.data.active_cryptocurrencies,
                 numExchanges: data.data.markets,
                 btcDom: data.data.market_cap_percentage.btc,
@@ -35,7 +34,10 @@ export default class Infographic extends React.Component {
         }
         catch (err) {
             console.log(err);
-            this.setState({isLoading: false})
+            this.setState({
+                isLoading: false,
+                hasError: true
+            })
         }
     }
     componentDidMount() {
