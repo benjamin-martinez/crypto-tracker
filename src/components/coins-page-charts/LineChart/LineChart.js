c6442fe5df82c38697b53ed5e3a580e3aaf55b9d
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-11-26 14:16:04 -0500

    Added red line and fill for negative trending chart

--- src/components/coins-page-charts/LineChart/LineChart.js
+++ src/components/coins-page-charts/LineChart/LineChart.js
@@ -19,14 +19,23 @@ export default class Chart extends React.Component {
         const chartData = (canvas) => {
             const ctx = canvas.getContext('2d')
             var gradientFill = ctx.createLinearGradient(0, 0, 0, 350);
-            gradientFill.addColorStop(0, "rgba(0, 255, 95, .5)");
-            gradientFill.addColorStop(1, "rgba(0, 0, 0, 0.0)");
+            let borderColor = ""
+            let prices = this.getPrices(this.props.coinPrices)
+            if (prices[0] > prices[prices.length-1]) {
+                borderColor =  "rgba(254, 16, 64, 1)"
+                gradientFill.addColorStop(0, "rgba(254, 16, 64, .5)");
+                gradientFill.addColorStop(1, "rgba(0, 0, 0, 0.0)");
+            } else {
+                borderColor = "rgba(0, 255, 95, 1)"
+                gradientFill.addColorStop(0, "rgba(0, 255, 95, .5)");
+                gradientFill.addColorStop(1, "rgba(0, 0, 0, 0.0)");
+            }
             return {
                 labels: this.getLabels(this.props.coinPrices),
                 datasets: [{
-                    data: this.getPrices(this.props.coinPrices),
+                    data: prices,
                     tension: 0.4,
-                    borderColor: "rgba(0, 255, 95, 1)",
+                    borderColor: borderColor,
                     fill: true,
                     backgroundColor: gradientFill
                 }],
