c58558c1576574f0dde9c0f77018370874dadf7e
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-12-01 14:14:15 -0500

    CoinPage table components switched over to hooks

--- src/components/coins-page-table/CoinTable/CoinTable.js
+++ src/components/coins-page-table/CoinTable/CoinTable.js
@@ -1,71 +1,65 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
 import { TableRow } from "components/coins-page-table";
-import { OutsideWrapper, Wrapper, HeaderRow } from "./CoinTable.styles";
 import { CoinTableTitle } from "styles/Fonts";
+import { OutsideWrapper, Wrapper, HeaderRow } from "./CoinTable.styles";
 
-export default class CoinTable extends React.Component {
-    state = {
-        coins: [],
-        isLoading: false,
-        hasError: false
-    }
+const CoinTable = (props) => {
+    const [coins, setCoins] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [hasError, setHasError] = useState(false)
 
-    getTableData = async () => {
-        this.setState({isLoading: true})
+    const getTableData = async () => {
+        setIsLoading(true)
         try {
             const { data } = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d");
-            console.log(data)
-            this.setState({
-                coins: data,
-                isLoading: false,
-                hasError: false
-            })
+            setCoins(data)
+            setIsLoading(false)
+            setHasError(false)
         }
         catch (err) {
-            this.setState({
-                isLoading: false,
-                hasError: true
-            })
+            setIsLoading(false)
+            setHasError(true)
         }
     }
     
-    componentDidMount() {
-        this.getTableData();
-    }
+    useEffect(() => {
+        getTableData();
+    }, [])
 
-    render() {
-        return (
-            <OutsideWrapper>
-            <Wrapper>
-                <col style={{width:"5.3%"}} />
-	            <col style={{width:"18.4%"}} />
-                <col style={{width:"7.9%"}} />
-                <col style={{width:"7.4%"}} />
-                <col style={{width:"8.3%"}} />
-                <col style={{width:"7.5%"}} />
-                <col style={{width:"18.7%"}} />
-                <col style={{width:"18.6%"}} />
-                <col style={{width:"8.6%"}} />
-                <HeaderRow>
-                    <tr>
-                        <th><CoinTableTitle>#</CoinTableTitle></th>
-                        <th><CoinTableTitle>Name</CoinTableTitle></th>
-                        <th><CoinTableTitle>Price</CoinTableTitle></th>
-                        <th><CoinTableTitle>1h%</CoinTableTitle></th>
-                        <th><CoinTableTitle>24h%</CoinTableTitle></th>
-                        <th><CoinTableTitle>7d%</CoinTableTitle></th>
-                        <th><CoinTableTitle>24h Volume/Market Cap</CoinTableTitle></th>
-                        <th><CoinTableTitle>Circulating/Total Supply</CoinTableTitle></th>
-                        <th><CoinTableTitle>Last 7d</CoinTableTitle></th>
-                    </tr>
-                </HeaderRow>
-                {this.state.coins.map(coin => <TableRow key={coin.id} coin={coin} />)}
-            </Wrapper>
-            {/* <Pagination>
-                
-            </Pagination> */}
-            </OutsideWrapper>
-        )
-    }
+    return (
+        <OutsideWrapper>
+        <Wrapper>
+            <col style={{width:"5.3%"}} />
+            <col style={{width:"18.4%"}} />
+            <col style={{width:"7.9%"}} />
+            <col style={{width:"7.4%"}} />
+            <col style={{width:"8.3%"}} />
+            <col style={{width:"7.5%"}} />
+            <col style={{width:"18.7%"}} />
+            <col style={{width:"18.6%"}} />
+            <col style={{width:"8.6%"}} />
+            <HeaderRow>
+                <tr>
+                    <th><CoinTableTitle>#</CoinTableTitle></th>
+                    <th><CoinTableTitle>Name</CoinTableTitle></th>
+                    <th><CoinTableTitle>Price</CoinTableTitle></th>
+                    <th><CoinTableTitle>1h%</CoinTableTitle></th>
+                    <th><CoinTableTitle>24h%</CoinTableTitle></th>
+                    <th><CoinTableTitle>7d%</CoinTableTitle></th>
+                    <th><CoinTableTitle>24h Volume/Market Cap</CoinTableTitle></th>
+                    <th><CoinTableTitle>Circulating/Total Supply</CoinTableTitle></th>
+                    <th><CoinTableTitle>Last 7d</CoinTableTitle></th>
+                </tr>
+            </HeaderRow>
+            {coins.map(coin => <TableRow key={coin.id} coin={coin} />)}
+        </Wrapper>
+        {/* <Pagination>
+            
+        </Pagination> */}
+        </OutsideWrapper>
+    )
+    
 }
+
+export default CoinTable
--- src/components/coins-page-table/TableRow/TableRow.js
+++ src/components/coins-page-table/TableRow/TableRow.js
@@ -7,43 +7,42 @@ import { UpArrow, DownArrow } from "styles/arrows";
 import { Slider, SliderWrapper } from "styles/sliders";
 import { addDecimalsAndShorten, addCommas, formatLargeNumber, formatPercentage } from "utils/formatPrice";
 
-export default class TableRow extends React.Component {
-
-    render() {
-        return (
-            <Wrapper >
-                <tr>
-                    <td><CoinTableRowText>{this.props.coin.market_cap_rank}</CoinTableRowText></td>
-                    <td><Link to={`/coin/${this.props.coin.id}`}><TokenSpan><Icon src={this.props.coin.image} /><CoinTableRowText>{this.props.coin.name} ({this.props.coin.symbol.toUpperCase()})</CoinTableRowText></TokenSpan></Link></td>
-                    <td><CoinTableRowText>{addCommas(this.props.coin.current_price)}</CoinTableRowText></td>
-                    <td><ChangeSpan>{this.props.coin.price_change_percentage_1h_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(this.props.coin.price_change_percentage_1h_in_currency)}</CoinTableRowText></ChangeSpan></td>
-                    <td><ChangeSpan>{this.props.coin.price_change_percentage_24h_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(this.props.coin.price_change_percentage_24h_in_currency)}</CoinTableRowText></ChangeSpan></td>
-                    <td><ChangeSpan>{this.props.coin.price_change_percentage_7d_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(this.props.coin.price_change_percentage_7d_in_currency)}</CoinTableRowText></ChangeSpan></td>
-                    <td>
-                        <DoubleSpan>
-                            <DoubleSpanTop>
-                                <CoinTableRowText>{addDecimalsAndShorten(this.props.coin.total_volume)}</CoinTableRowText>
-                                <CoinTableRowText>{addDecimalsAndShorten(this.props.coin.market_cap)}</CoinTableRowText>
-                            </DoubleSpanTop>
-                            <SliderWrapper height="8px" width="100%" background="white">
-                                <Slider width={(this.props.coin.total_volume/this.props.coin.market_cap)*100} background="#aaaaaa" />
-                            </SliderWrapper>
-                        </DoubleSpan>
-                    </td>
-                    <td>
-                        <DoubleSpan>
-                            <DoubleSpanTop>
-                                <CoinTableRowText>{formatLargeNumber(this.props.coin.circulating_supply)}</CoinTableRowText>
-                                <CoinTableRowText>{this.props.coin.total_supply === null ? "∞" : formatLargeNumber(this.props.coin.total_supply)}</CoinTableRowText>
-                            </DoubleSpanTop>
-                            <SliderWrapper height="8px" width="100%" background="white">
-                                <Slider width={(this.props.coin.circulating_supply/this.props.coin.total_supply)*100} background="#aaaaaa" />
-                            </SliderWrapper>
-                        </DoubleSpan>
-                    </td>
-                    <td><Sparkline prices={this.props.coin.sparkline_in_7d.price}/></td>
-                </tr>
-            </Wrapper>
-        )
-    }
+const TableRow = (props) => {
+    return (
+        <Wrapper >
+            <tr>
+                <td><CoinTableRowText>{props.coin.market_cap_rank}</CoinTableRowText></td>
+                <td><Link to={`/coin/${props.coin.id}`}><TokenSpan><Icon src={props.coin.image} /><CoinTableRowText>{props.coin.name} ({props.coin.symbol.toUpperCase()})</CoinTableRowText></TokenSpan></Link></td>
+                <td><CoinTableRowText>{addCommas(props.coin.current_price)}</CoinTableRowText></td>
+                <td><ChangeSpan>{props.coin.price_change_percentage_1h_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(props.coin.price_change_percentage_1h_in_currency)}</CoinTableRowText></ChangeSpan></td>
+                <td><ChangeSpan>{props.coin.price_change_percentage_24h_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(props.coin.price_change_percentage_24h_in_currency)}</CoinTableRowText></ChangeSpan></td>
+                <td><ChangeSpan>{props.coin.price_change_percentage_7d_in_currency > 0 ? <UpArrow /> : <DownArrow/>}<CoinTableRowText>{formatPercentage(props.coin.price_change_percentage_7d_in_currency)}</CoinTableRowText></ChangeSpan></td>
+                <td>
+                    <DoubleSpan>
+                        <DoubleSpanTop>
+                            <CoinTableRowText>{addDecimalsAndShorten(props.coin.total_volume)}</CoinTableRowText>
+                            <CoinTableRowText>{addDecimalsAndShorten(props.coin.market_cap)}</CoinTableRowText>
+                        </DoubleSpanTop>
+                        <SliderWrapper height="8px" width="100%" background="white">
+                            <Slider width={(props.coin.total_volume/props.coin.market_cap)*100} background="#aaaaaa" />
+                        </SliderWrapper>
+                    </DoubleSpan>
+                </td>
+                <td>
+                    <DoubleSpan>
+                        <DoubleSpanTop>
+                            <CoinTableRowText>{formatLargeNumber(props.coin.circulating_supply)}</CoinTableRowText>
+                            <CoinTableRowText>{props.coin.total_supply === null ? "∞" : formatLargeNumber(props.coin.total_supply)}</CoinTableRowText>
+                        </DoubleSpanTop>
+                        <SliderWrapper height="8px" width="100%" background="white">
+                            <Slider width={(props.coin.circulating_supply/props.coin.total_supply)*100} background="#aaaaaa" />
+                        </SliderWrapper>
+                    </DoubleSpan>
+                </td>
+                <td><Sparkline prices={props.coin.sparkline_in_7d.price}/></td>
+            </tr>
+        </Wrapper>
+    )
 }
+
+export default TableRow
