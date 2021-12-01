856811eb7123aab4a0b8c3ab62db4a37bdd507a8
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-12-01 16:01:07 -0500

    converted app.js file to hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { ThemeProvider } from "styled-components";
 import {
   BrowserRouter as Router,
@@ -9,35 +9,32 @@ import { GlobalStyle } from "styles/GlobalStyle";
 import { themes } from "styles/colors";
 import Header from "components/header/Header/Header";
 
-class App extends React.Component {
-  state = {
-    theme: themes.dark
-  }
-  toggleTheme = () => {
-    if(this.state.theme === themes.dark) {
-      this.setState({theme: themes.light})
+function App() {
+  const [theme, setTheme] = useState(themes.dark)
+
+  const toggleTheme = () => {
+    if(theme === themes.dark) {
+      setTheme(themes.light)
     } else {
-      this.setState({theme: themes.dark})
+      setTheme(themes.dark)
     }
   }
-  render() {
-    return (
-      <ThemeProvider theme={this.state.theme}>
-        <div className="App">
-          <GlobalStyle theme={this.state.theme}/>
-          <Router >
-            <Header toggleTheme={this.toggleTheme}/>
-            <Switch>
-              <Route path="/coins" component={Coins} />
-              <Route path="/portfolio" component={Portfolio} />
-              <Route path="/coin/:id" component={Coin} />
-              <Route path="*" render={() => <Redirect to="/coins" component={Coins} />}/>
-            </Switch>
-          </Router>
-        </div>
-      </ThemeProvider>
-    );
-  }
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle theme={theme}/>
+        <Router >
+          <Header toggleTheme={toggleTheme}/>
+          <Switch>
+            <Route path="/coins" component={Coins} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/coin/:id" component={Coin} />
+            <Route path="*" render={() => <Redirect to="/coins" component={Coins} />}/>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App;
