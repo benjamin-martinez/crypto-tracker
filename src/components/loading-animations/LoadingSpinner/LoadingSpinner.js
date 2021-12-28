0d247132142e53a90bffc7697813a77b0ac5e4dc
Benjamin Martinez <benjamin.martiinez@gmail.com>
2021-12-28 14:50:27 -0500

    removed unused code and console logs

--- src/components/loading-animations/LoadingSpinner/LoadingSpinner.js
+++ src/components/loading-animations/LoadingSpinner/LoadingSpinner.js
@@ -1,12 +1,7 @@
 import { Spinner, Wrapper } from "./LoadingSpinner.styles";
-import { useWindowSize } from "hooks";
 import { getWidth, getHeight } from "utils";
 
 const LoadingSpinner = () => {
-  const { width: screenWidth } = useWindowSize();
-  console.log(window.innerWidth);
-  console.log(getWidth(window.innerWidth));
-  console.log(getHeight(window.innerWidth));
   return (
     <Wrapper
       width={getWidth(window.innerWidth)}
